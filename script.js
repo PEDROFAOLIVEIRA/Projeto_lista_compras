c61c399ecb3e5c44b7a7248001e7c958bc961371
Pedro de Oliveira <pedrofelipee@gmail.com>
2026-09-09 17:33:59 -0300

    script inicio

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const header = document.querySelector('header');
 const nav = document.querySelector('nav');
 const container = document.querySelector('.container'); /*quem que colocar o ponto antes do nome da classe*/
 const list = document.querySelector('.list');
+const hortifrute = document.querySelector('.hortifrute');
+const carnes = document.querySelector('.carnes');
+const bebidas = document.querySelector('.bebidas');
+const materialDeLimpeza = document.querySelector('.materialDeLimpeza');
