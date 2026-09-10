c51a6b1a10a8bb13b8a9465bd1a82a6c2d244e4e
Pedro de Oliveira <pedrofelipee@gmail.com>
2026-09-10 13:52:29 -0300

    ajuste nav e otimizacao no script - exclusão da const header e nav

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const header = document.querySelector('header');
-const nav = document.querySelector('nav');
 const container = document.querySelector('.container'); /*quem que colocar o ponto antes do nome da classe*/
 const list = document.querySelector('.list');
 const hortifrute = document.querySelector('.hortifrute');
