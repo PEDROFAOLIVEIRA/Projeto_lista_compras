a3c9ea14c6b6ffcdba707244fba4379f92cc2ea0
Pedro de Oliveira <pedrofelipee@gmail.com>
2026-09-12 13:02:54 -0300

    Correção no HTML

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const hortifrute = document.querySelector('.hortifrute');
 const carnes = document.querySelector('.carnes');
 const bebidas = document.querySelector('.bebidas');
 const materialDeLimpeza = document.querySelector('.materialDeLimpeza');
+const botao = document.querySelector ('button');
+/*Acima, estou selecionando os elementos do HTML que vou precisar manipular no JS, de forma global */
+function adicionarItem() {
+  const textarea = window.prompt;
+        
+  window.alert(textarea.value);
+}
